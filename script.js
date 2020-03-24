1fb15263ac584ca70399800e021907301a7f8f59
Daniil Fomin <40365333+donnyqqq@users.noreply.github.com>
2020-03-24 21:14:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+function plus1() {
+  let num = document.getElementById('num').innerHTML;
+  let a = parseInt(num);
+  document.getElementById('num').innerHTML = a + 1;
+}
+
+function plus2() {
+  let num = document.getElementById('num').innerHTML;
+  let a = parseInt(num);
+  document.getElementById('num').innerHTML = a + 2;
+}
+
+function plus5() {
+  let num = document.getElementById('num').innerHTML;
+  let a = parseInt(num);
+  document.getElementById('num').innerHTML = a + 5;
+}
+
+function plus10() {
+  let num = document.getElementById('num').innerHTML;
+  let a = parseInt(num);
+  document.getElementById('num').innerHTML = a + 10;
+}
+
+function plus50() {
+  let num = document.getElementById('num').innerHTML;
+  let a = parseInt(num);
+  document.getElementById('num').innerHTML = a + 50;
+}
+
+function plus100() {
+  let num = document.getElementById('num').innerHTML;
+  let a = parseInt(num);
+  document.getElementById('num').innerHTML = a + 100;
+}
+
